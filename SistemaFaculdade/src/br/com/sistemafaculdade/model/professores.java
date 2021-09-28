60fdd209266ae10093a625e58ebf736032bfd70a
Andrey Gheno Piekas <andrey.piekas@gmail.com>
2021-09-28 20:12:00 -0300

    Delete SistemaFaculdade directory

--- SistemaFaculdade/src/br/com/sistemafaculdade/model/professores.java
+++
@@ -1,6 +0,0 @@
-package br.com.sistemafaculdade.model;
-
-public class professores extends administrativo {
-
-
-}
--- SistemaFaculdade/src/br/com/sistemafaculdade/model/servicosGerais.java
+++
@@ -1,6 +0,0 @@
-package br.com.sistemafaculdade.model;
-
-public class servicosGerais extends administrativo {
-
-
-}
